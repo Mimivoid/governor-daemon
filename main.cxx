5077181c3f1041a6d71723565e97cc35910296de
Mimivoid <mimivoid128@proton.me>
2026-01-16 00:12:34 -0500

    Optimize, change parameters of daemon

--- main.cxx
+++ main.cxx
@@ -5,8 +5,6 @@ extern "C" {
 #include <hwloc.h>
 #include <time.h>
 
-using namespace std;
-
 // Init
 int main() {
 
@@ -16,11 +14,8 @@ int main() {
 	hwloc_topology_t topology;
 	hwloc_topology_init(&topology);
 	hwloc_topology_load(topology);
-
-	hwloc_const_cpuset_t cpuset;
-	cpuset = hwloc_topology_get_complete_cpuset(topology);
 	
-	int ncpus = hwloc_bitmap_weight(cpuset);
+	int ncpus = hwloc_bitmap_weight(hwloc_topology_get_complete_cpuset(topology));
 
 	printf("%d\n", ncpus);
 
@@ -49,24 +44,24 @@ int main() {
 			printf("%s\n", policy->governor);
 			printf ("%d\n", current_freq);
 			// below 3633mhz set to powersave
-			if (current_freq < max_freq / 1.22) {
+			if (current_freq < max_freq / 1.259) {
 				cpufreq_modify_policy_governor(cpu_count, gov_save);
 			}
 			
 			// if below 4.1ghz set cstates to all enable
-			if (current_freq < max_freq / 1.36) {
+			if (current_freq < max_freq / 1.23) {
 				for (int cstate_num = 0; cstate_num <= 15; ++cstate_num) {
 					cpuidle_state_disable(cpu_count, cstate_num, 0);
 				}
 			}
 			
 			// above 3633mhz set to performance
-			if (current_freq > max_freq / 1.2) {
+			if (current_freq > max_freq / 1.184) {
 				cpufreq_modify_policy_governor(cpu_count, gov_perf);
 			}
 
 			// if above 4.1ghz set cstates 7-15 disable
-			if (current_freq > max_freq / 1.36) {
+			if (current_freq > max_freq / 1.23) {
 				for (int cstate_num = 4; cstate_num <= 15; ++cstate_num) {
 					cpuidle_state_disable(cpu_count, cstate_num, 1);
 				}
